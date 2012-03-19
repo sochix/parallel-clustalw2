ad7cc0d31ce1764db81ebcf511218da9e91050b6
Ilya <ilya.pir@gmail.com>
2012-03-19 13:51:55 -0700

    Stuff for testing correctness of ClustalW2 results

---
+++ test_clustal.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+path_to_clustal=./src/clustalw2
+path_to_source=./src/PfamTest.fasta
+path_to_result=./src/PfamTest.aln
+path_to_sample=./sample/PfamTest.aln
+
+echo "All right script started..."
+echo `$path_to_clustal $path_to_source` > log.txt
+if ! diff $path_to_sample $path_to_result; then
+	echo "ERROR! Files NOT equal!"
+	exit 1
+else
+	echo "Ok. Files equal"
+fi
+exit 0
