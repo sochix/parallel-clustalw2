9573e506311a6b417a6a1c16813217717c06da0e
Ilya Pirozhenko <ilya.pir@gmail.com>
2012-11-05 10:52:36 +0400

    Renaming of variables in MM algo

--- src/pairwise/I_MMAlgo.cpp
+++ src/pairwise/I_MMAlgo.cpp
@@ -10,7 +10,7 @@ MMAlgo::MMAlgo():
   _gapOpen(0),
   _gapExtend(0)
 {
-	displ.resize((2 * ExtendData::maxAlnLength) + 1);
+  displ.resize((2 * ExtendData::maxAlnLength) + 1);
   HH.resize(ExtendData::maxAlnLength);
   DD.resize(ExtendData::maxAlnLength);
   RR.resize(ExtendData::maxAlnLength);
@@ -19,14 +19,14 @@ MMAlgo::MMAlgo():
 
 MMAlgo::~MMAlgo()
 {
-	  displ.clear();
+    displ.clear();
     HH.clear();
     DD.clear();
     RR.clear();
     SS.clear();
 }
 
-int MMAlgo::Pass(int A, int B, int M, int N, int tb, int te, const vector<int>* seq1, const vector<int>* seq2, const int gapOpen, const int gapExtend)
+int MMAlgo::Pass(int sb1, int sb2, int len1, int len2, int tb, int te, const vector<int>* seq1, const vector<int>* seq2, const int gapOpen, const int gapExtend)
 {
 		_ptrToSeq1 = seq1;
 		_ptrToSeq2 = seq2;
@@ -36,7 +36,7 @@ int MMAlgo::Pass(int A, int B, int M, int N, int tb, int te, const vector<int>* 
 		lastPrint = 0;
 		printPtr = 1;
 		
-		return diff(A,B,M,N,tb,te);
+		return diff(sb1,sb2,len1,len2,tb,te);
 		
 }
 
@@ -58,41 +58,41 @@ void MMAlgo::add(int v)
     }
 }
 
-int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
+int MMAlgo::diff(int sb1, int sb2, int len1, int len2, int tb, int te)
 {
     int type;
     int midi, midj, i, j;
     int midh;
   //  static int f, hh, e, s, t;
 
-    if (N <= 0)
+    if (len2 <= 0)
     {
-        if (M > 0)
+        if (len1 > 0)
         {
-            del(M);
+            del(len1);
         }
 
-        return ( -(int)tbgap(M, tb));
+        return ( -(int)tbgap(len1, tb));
     }
 
-    if (M <= 1)
+    if (len1 <= 1)
     {
-        if (M <= 0)
+        if (len1 <= 0)
         {
-            add(N);
-            return ( -(int)tbgap(N, tb));
+            add(len2);
+            return ( -(int)tbgap(len2, tb));
         }
 
-        midh =  - (tb + _gapExtend) - tegap(N, te);
-        hh =  - (te + _gapExtend) - tbgap(N, tb);
+        midh =  - (tb + _gapExtend) - tegap(len2, te);
+        hh =  - (te + _gapExtend) - tbgap(len2, tb);
         if (hh > midh)
         {
             midh = hh;
         }
         midj = 0;
-        for (j = 1; j <= N; j++)
+        for (j = 1; j <= len2; j++)
         {
-            hh = calcScore(1, j, A, B) - tegap(N - j, te) - tbgap(j - 1, tb);
+            hh = calcScore(1, j, sb1, sb2) - tegap(len2 - j, te) - tbgap(j - 1, tb);
             if (hh > midh)
             {
                 midh = hh;
@@ -103,7 +103,7 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
         if (midj == 0)
         {
             del(1);
-            add(N);
+            add(len2);
         }
         else
         {
@@ -112,9 +112,9 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
                 add(midj - 1);
             }
             displ[printPtr++] = lastPrint = 0;
-            if (midj < N)
+            if (midj < len2)
             {
-                add(N - midj);
+                add(len2 - midj);
             }
         }
         return midh;
@@ -122,10 +122,10 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
 
     // Divide: Find optimum midpoint (midi,midj) of cost midh
 
-    midi = M / 2;
+    midi = len1 / 2;
     HH[0] = 0;
     t =  - tb;
-    for (j = 1; j <= N; j++)
+    for (j = 1; j <= len2; j++)
     {
         HH[j] = t = t - _gapExtend;
         DD[j] = t - _gapOpen;
@@ -137,7 +137,7 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
         s = HH[0];
         HH[0] = hh = t = t - _gapExtend;
         f = t - _gapOpen;
-        for (j = 1; j <= N; j++)
+        for (j = 1; j <= len2; j++)
         {
             if ((hh = hh - _gapOpen - _gapExtend) > (f = f - _gapExtend))
             {
@@ -147,7 +147,7 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
             {
                 e = hh;
             }
-            hh = s + calcScore(i, j, A, B);
+            hh = s + calcScore(i, j, sb1, sb2);
             if (f > hh)
             {
                 hh = f;
@@ -165,22 +165,22 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
 
     DD[0] = HH[0];
 
-    RR[N] = 0;
+    RR[len2] = 0;
     t =  - te;
-    for (j = N - 1; j >= 0; j--)
+    for (j = len2 - 1; j >= 0; j--)
     {
         RR[j] = t = t - _gapExtend;
         SS[j] = t - _gapOpen;
     }
 
     t =  - te;
-    for (i = M - 1; i >= midi; i--)
+    for (i = len1 - 1; i >= midi; i--)
     {
-        s = RR[N];
-        RR[N] = hh = t = t - _gapExtend;
+        s = RR[len2];
+        RR[len2] = hh = t = t - _gapExtend;
         f = t - _gapOpen;
 
-        for (j = N - 1; j >= 0; j--)
+        for (j = len2 - 1; j >= 0; j--)
         {
 
             if ((hh = hh - _gapOpen - _gapExtend) > (f = f - _gapExtend))
@@ -191,7 +191,7 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
             {
                 e = hh;
             }
-            hh = s + calcScore(i + 1, j + 1, A, B);
+            hh = s + calcScore(i + 1, j + 1, sb1, sb2);
             if (f > hh)
             {
                 hh = f;
@@ -208,12 +208,12 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
         }
     }
 
-    SS[N] = RR[N];
+    SS[len2] = RR[len2];
 
     midh = HH[0] + RR[0];
     midj = 0;
     type = 1;
-    for (j = 0; j <= N; j++)
+    for (j = 0; j <= len2; j++)
     {
         hh = HH[j] + RR[j];
         if (hh >= midh)
@@ -224,7 +224,7 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
         }
     }
 
-    for (j = N; j >= 0; j--)
+    for (j = len2; j >= 0; j--)
     {
         hh = DD[j] + SS[j] + _gapOpen;
         if (hh > midh)
@@ -241,14 +241,14 @@ int MMAlgo::diff(int A, int B, int M, int N, int tb, int te)
     if (type == 1)
     {
         // Type 1 gaps
-        diff(A, B, midi, midj, tb, _gapOpen);
-        diff(A + midi, B + midj, M - midi, N - midj, _gapOpen, te);
+        diff(sb1, sb2, midi, midj, tb, _gapOpen);
+        diff(sb1 + midi, sb2 + midj, len1 - midi, len2 - midj, _gapOpen, te);
     }
     else
     {
-        diff(A, B, midi - 1, midj, tb, 0);
+        diff(sb1, sb2, midi - 1, midj, tb, 0);
         del(2);
-        diff(A + midi + 1, B + midj, M - midi - 1, N - midj, 0, te);
+        diff(sb1 + midi + 1, sb2 + midj, len1 - midi - 1, len2 - midj, 0, te);
     }
 
     return midh; // Return the score of the best alignment
